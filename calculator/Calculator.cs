f5842dbbad5551daa14386e43ed8429042715222
mxmostap <mxmostapwork@gmail.com>
2025-10-03 20:22:00 +0300

    Initial commit

---
+++ calculator/Calculator.cs
@@ -0,0 +1,83 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace calculator;
+
+public class Calculator
+{
+    public static double GetNumber(string message)
+    {
+        while (true)
+        {
+            Console.Write(message);
+            double number;
+            if (double.TryParse(GetConsoleInput(@"\d+\,?\d+"), out number))
+                return number;
+            else 
+                Console.WriteLine("Ошибка ввода - Вы ввели не число. Повторите ввод.");
+        }
+    }
+
+    static string GetConsoleInput(string rule)
+    {
+        ConsoleKeyInfo key;
+        var input = "";
+        while(true)
+        {
+            key = Console.ReadKey(true);
+            if(key.Key == ConsoleKey.Enter)
+                break;
+            if(Regex.IsMatch(key.KeyChar.ToString(), rule))
+            {
+                input += key.KeyChar;
+                Console.Write(key.KeyChar);
+            }
+        }
+
+        return input;
+    }
+    public static string GetSystemSeparator()
+    {
+        CultureInfo currentCulture = CultureInfo.CurrentCulture;
+        return currentCulture.NumberFormat.NumberDecimalSeparator;
+    }
+
+    public static string GetOperation()
+    {
+        //string[] validOperations = {"+", "-", "*", "/"};
+        while (true)
+        {
+            Console.WriteLine("Доступные арифметические операции: '+','-','*','/'.");
+            Console.Write("Введите арифметическую операцию: ");
+            //string? input = Console.ReadLine().Replace(" ", "");
+            string input = GetConsoleInput(@"[+-*/]");
+            //if (validOperations.Contains(input)) 
+              //  return input;    
+            //else
+              //  Console.WriteLine("Ошибка ввода. Введите операцию из списка доступных.");
+              return input;
+        }
+    }
+    
+    public static void GetResult(double num1, double num2, string operation)
+    {
+        switch (operation)
+        {
+            case "+": 
+                Console.WriteLine($"Результат операции: {num1 + num2}"); 
+                break;
+            case "-": 
+                Console.WriteLine($"Результат операции: {num1 - num2}"); 
+                break;
+            case "*": 
+                Console.WriteLine($"Результат операции: {num1 * num2}"); 
+                break;
+            case "/": 
+                Console.WriteLine($"Результат операции: {num1 / num2}"); 
+                break;
+            default: 
+                Console.WriteLine($"Результат операции: 0");
+                break;
+        }
+    }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Вас привествует консольный калькулятор!");
+            Console.WriteLine($"Вещественные числа необходимо вводить через символ: '{Calculator.GetSystemSeparator()}'");
+            Console.WriteLine();
+            try
+            {
+                double number1 = Calculator.GetNumber("Введите первое число:");
+                string operation = Calculator.GetOperation();
+                double number2 = Calculator.GetNumber("Введите второе число:");
+                if (operation == "/" & number2 == 0) 
+                {
+                    Console.WriteLine("ОШИБКА: Делить на ноль нельзя!");
+                    Console.Write("Чтобы выйти из программы введите 'exit',\n чтобы продолжить введите любые символы: ");
+                    if (Console.ReadLine() == "exit") break;
+                    else
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                }
+                Calculator.GetResult(number1, number2, operation);
+
+                Console.Write("Чтобы выйти из программы введите 'exit',\n чтобы продолжить введите любые символы: ");
+                if (Console.ReadLine() == "exit") break;
+                else
+                {
+                    Console.Clear();
+                    continue;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Вызвано исключение: {1}", e.Message);
+            }
+        }
+    }
+}
